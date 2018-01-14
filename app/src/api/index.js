5d16adfb310e8d8e1f6604bba833fe026f84d8e6
wxj <139439432@qq.com>
2018-01-15 03:10:10 +0800

    115

---
+++ app/src/api/index.js
@@ -0,0 +1,11 @@
+import wepy from 'wepy'
+
+export function wxhttp(url, params) {
+    wepy.request({
+        url: 'https://www.madcoder.cn/tests/sleep.php?time=1&t=css&c=' + map[i] + '&i=' + i,
+        success: function (d) {
+        //   self.netrst += d.data + '.'
+        //   self.$apply()
+        }
+      })
+}
